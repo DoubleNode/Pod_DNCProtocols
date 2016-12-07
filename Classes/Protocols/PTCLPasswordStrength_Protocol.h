414987f9071dd8c9f8c2eadf0b875580f7dfbe79
Darren Ehlers <me@darrenehlers.com>
2016-12-07 11:50:07 -0600

    Added PasswordStrength protocol and update podspec version
    Signed-off-by: Darren Ehlers <me@darrenehlers.com>

---
+++ Classes/Protocols/PTCLPasswordStrength_Protocol.h
@@ -0,0 +1,29 @@
+//
+//  PTCLPasswordStrength_Protocol.h
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#import "__PTCLBase_Protocol.h"
+
+typedef NS_ENUM(NSUInteger, WKRPasswordStrengthType)
+{
+    WKRPasswordStrengthTypeWeak,
+    WKRPasswordStrengthTypeModerate,
+    WKRPasswordStrengthTypeStrong
+};
+
+@protocol PTCLPasswordStrength_Protocol <PTCLBase_Protocol>
+
+@property (strong, nonatomic)   id<PTCLPasswordStrength_Protocol> _Nullable nextPasswordStrengthWorker;
+
++ (nonnull instancetype)worker;
++ (nonnull instancetype)worker:(nullable id<PTCLPasswordStrength_Protocol>)nextWorker;
+
+#pragma mark - Business Logic
+
+- (WKRPasswordStrengthType)doCheckPasswordStrength:(nonnull NSString*)password;
+
+@end
