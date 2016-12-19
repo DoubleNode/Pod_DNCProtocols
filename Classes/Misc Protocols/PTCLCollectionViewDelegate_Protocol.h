ebf04fd0ccdc7e40136ec080359e6ece939ea523
Darren Ehlers <me@darrenehlers.com>
2016-12-19 00:08:37 -0600

    Added CollectionViewDelegate protocol

---
+++ Classes/Misc Protocols/PTCLCollectionViewDelegate_Protocol.h
@@ -0,0 +1,22 @@
+//
+//  PTCLCollectionViewDelegate_Protocol.h
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#ifndef PTCLCollectionViewDelegate_Protocol_h
+#define PTCLCollectionViewDelegate_Protocol_h
+
+#import <UIKit/UIKit.h>
+
+@protocol PTCLCollectionViewDelegate_Protocol <UICollectionViewDelegate>
+
++ (nonnull instancetype)delegateWithCollectionView:(nullable UICollectionView*)collectionView;
+
+- (nonnull instancetype)initWithCollectionView:(nullable UICollectionView*)collectionView;
+
+@end
+
+#endif /* PTCLCollectionViewDelegate_Protocol_h */
