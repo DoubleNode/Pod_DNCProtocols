1fd97078273c8355172e8b2d5a1e1107bf738cee
Darren Ehlers <me@darrenehlers.com>
2017-01-06 22:23:55 -0600

    Added PTCLState_Protocol and updated podspec version

---
+++ Classes/Misc Protocols/PTCLState_Protocol.h
@@ -0,0 +1,20 @@
+//
+//  PTCLState_Protocol.h
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#ifndef PTCLState_Protocol_h
+#define PTCLState_Protocol_h
+
+#import <UIKit/UIKit.h>
+
+@protocol PTCLState_Protocol
+
+@property (strong, nonatomic) NSDictionary* state;
+
+@end
+
+#endif /* PTCLState_Protocol_h */
