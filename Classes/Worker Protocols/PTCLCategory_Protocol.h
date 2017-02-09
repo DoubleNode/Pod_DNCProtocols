341b25345797be627d48448e683da3218e75eb14
Darren Ehlers <me@darrenehlers.com>
2017-02-08 22:37:46 -0600

    Setup new Category Protocol

--- Classes/Worker Protocols/PTCLCategory_Protocol.h
+++ Classes/Worker Protocols/PTCLCategory_Protocol.h
@@ -12,12 +12,26 @@
 #import "__PTCLBase_Protocol.h"
 
 @class DAOCategory;
+@class DAOItem;
+@class DAOPhoto;
 
 typedef void(^PTCLCategoryContinueBlock)();
 
-typedef void(^PTCLCategoryBlockVoidNSArrayNSUIntegerNSUIntegerNSError)(NSArray<DAOCategory* >* _Nullable categories, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error);
+typedef void(^PTCLCategoryBlockVoidNSError)(NSError* _Nullable error);
 
-typedef void(^PTCLCategoryBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)(NSArray<DAOCategory* >* _Nullable categories, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error, PTCLCategoryContinueBlock _Nullable continueBlock);
+typedef void(^PTCLCategoryBlockVoidBOOLNSError)(BOOL success, NSError* _Nullable error);
+typedef void(^PTCLCategoryBlockVoidDAOCategoryNSError)(DAOCategory* _Nullable category, NSError* _Nullable error);
+typedef void(^PTCLCategoryBlockVoidNSArrayDAOItemNSUIntegerNSUIntegerNSError)(NSArray<DAOItem* >* _Nullable items, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error);
+typedef void(^PTCLCategoryBlockVoidNSArrayDAOPhotoNSUIntegerNSUIntegerNSError)(NSArray<DAOPhoto* >* _Nullable photos, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error);
+typedef void(^PTCLCategorySearchBlockVoidNSArrayNSUIntegerNSUIntegerNSError)(NSString* _Nonnull searchId, NSArray<DAOCategory* >* _Nullable categories, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error);
+
+typedef void(^PTCLCategoryBlockVoidDAOCategoryNSErrorContinue)(DAOCategory* _Nullable category, NSError* _Nullable error, PTCLCategoryContinueBlock _Nullable continueBlock);
+typedef void(^PTCLCategoryBlockVoidNSArrayDAOItemNSUIntegerNSUIntegerNSErrorContinue)(NSArray<DAOItem* >* _Nullable items, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error, PTCLCategoryContinueBlock _Nullable continueBlock);
+typedef void(^PTCLCategoryBlockVoidNSArrayDAOPhotoNSUIntegerNSUIntegerNSErrorContinue)(NSArray<DAOPhoto* >* _Nullable photos, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error, PTCLCategoryContinueBlock _Nullable continueBlock);
+typedef void(^PTCLCategorySearchBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)(NSString* _Nonnull searchId, NSArray<DAOCategory* >* _Nullable categories, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error, PTCLCategoryContinueBlock _Nullable continueBlock);
+
+typedef void(^PTCLCategoryBlockVoidDAOLocation)(DAOCategory* _Nullable category);
+typedef void(^PTCLCategoryBlockVoidNSArrayNSUIntegerNSUIntegerNSError)(NSArray<DAOCategory* >* _Nullable categories, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error);
 
 @protocol PTCLCategory_Protocol <PTCLBase_Protocol>
 
@@ -26,11 +40,49 @@ typedef void(^PTCLCategoryBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)(N
 + (nonnull instancetype)worker;
 + (nonnull instancetype)worker:(nullable id<PTCLCategory_Protocol>)nextCategoryWorker;
 
+#pragma mark - Business Logic / Single Item CRUD
+
+- (void)doLoadObjectForId:(nonnull NSString*)categoryId
+                withBlock:(nullable PTCLCategoryBlockVoidDAOCategoryNSErrorContinue)block
+           andUpdateBlock:(nullable PTCLCategoryBlockVoidDAOCategoryNSError)updateBlock;
+
+- (void)doSaveObject:(nonnull DAOCategory*)category
+           withBlock:(nullable PTCLCategoryBlockVoidDAOCategoryNSError)block;
+
+- (void)doSaveObjectOptions:(nonnull DAOCategory*)category
+                  withBlock:(nullable PTCLCategoryBlockVoidBOOLNSError)block;
+
+- (void)doSaveOption:(nonnull NSString*)optionId
+              andKey:(nonnull NSString*)optionKey
+            andValue:(nullable id)optionValue
+         forLocation:(nonnull DAOCategory*)daoCategory
+           withBlock:(nullable PTCLCategoryBlockVoidBOOLNSError)block;
+
+- (void)doFollowObject:(nonnull DAOCategory*)category
+             withBlock:(nullable PTCLCategoryBlockVoidNSError)block;
+
+- (void)doUnfollowObject:(nonnull DAOCategory*)category
+               withBlock:(nullable PTCLCategoryBlockVoidNSError)block;
+
+#pragma mark - Business Logic / Single Item Relationship CRUD
+
+- (void)doLoadItemsForObject:(nonnull DAOCategory*)category
+                   withBlock:(nullable PTCLCategoryBlockVoidNSArrayDAOItemNSUIntegerNSUIntegerNSErrorContinue)block
+              andUpdateBlock:(nullable PTCLCategoryBlockVoidNSArrayDAOItemNSUIntegerNSUIntegerNSError)updateBlock;
+
+- (void)doLoadPhotosForObject:(nonnull DAOCategory*)category
+                    withBlock:(nullable PTCLCategoryBlockVoidNSArrayDAOPhotoNSUIntegerNSUIntegerNSErrorContinue)block
+               andUpdateBlock:(nullable PTCLCategoryBlockVoidNSArrayDAOPhotoNSUIntegerNSUIntegerNSError)updateBlock;
+
 #pragma mark - Business Logic / Collection Items CRUD
 
-- (void)doLoadItemsWithParameters:(nullable NSDictionary*)parameters
-                         andBlock:(nullable PTCLCategoryBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)block
-                   andUpdateBlock:(nullable PTCLCategoryBlockVoidNSArrayNSUIntegerNSUIntegerNSError)updateBlock;
+- (void)doLoadObjects:(nonnull NSString*)searchId
+             withText:(nonnull NSString*)search
+        withLongitude:(nullable NSNumber*)longitude
+         withLatitude:(nullable NSNumber*)latitude
+       withParameters:(nullable NSDictionary*)parameters
+             andBlock:(nullable PTCLCategorySearchBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)block
+       andUpdateBlock:(nullable PTCLCategorySearchBlockVoidNSArrayNSUIntegerNSUIntegerNSError)updateBlock;
 
 @end
 
--- DNCProtocols.podspec
+++ DNCProtocols.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "DNCProtocols"
-  s.version      = "0.0.30"
+  s.version      = "0.0.31"
   s.summary      = "A base collection of DNCProtocols headers."
 
   # This description is used to generate tags and improve search results.
