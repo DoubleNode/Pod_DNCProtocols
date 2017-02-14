2257a038f12dfd38ba2fcfa16d5d7f7393eefa29
Darren Ehlers <me@darrenehlers.com>
2017-02-14 11:20:39 -0600

    Added Cache Protocol and updated podspec version

---
+++ Classes/Worker Protocols/PTCLCache_Protocol.h
@@ -0,0 +1,38 @@
+//
+//  PTCLCache_Protocol.h
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#ifndef PTCLCache_Protocol_h
+#define PTCLCache_Protocol_h
+
+#import "__PTCLBase_Protocol.h"
+
+typedef void(^PTCLCacheBlockVoidNSError)(NSError* _Nullable error);
+typedef void(^PTCLCacheBlockVoidIDNSError)(ID _Nullable object, NSError* _Nullable error);
+
+@protocol PTCLCache_Protocol <PTCLBase_Protocol>
+
+@property (strong, nonatomic)   id<PTCLCache_Protocol> _Nullable  nextCacheWorker;
+
++ (nonnull instancetype)worker;
++ (nonnull instancetype)worker:(nullable id<PTCLCache_Protocol>)nextCacheWorker;
+
+#pragma mark - Business Logic / Single Item CRUD
+
+- (void)doLoadObjectForId:(nonnull NSString*)cacheId
+                withBlock:(nullable PTCLCacheBlockVoidIDNSError)block;
+
+- (void)doDeleteObjectForId:(nonnull NSString*)cacheId
+                  withBlock:(nullable PTCLCacheBlockVoidNSError)block;
+
+- (void)doSaveObject:(nonnull id)object
+               forId:(nonnull NSString*)cacheId
+           withBlock:(nullable PTCLCacheBlockVoidNSError)block;
+
+@end
+
+#endif /* PTCLCache_Protocol_h */
