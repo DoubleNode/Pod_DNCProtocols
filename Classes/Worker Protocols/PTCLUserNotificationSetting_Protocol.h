8037779eed245fdc154c621e1294e93534f67732
Darren Ehlers <me@darrenehlers.com>
2017-04-27 22:47:47 -0500

    Added UserNotificationSetting protocol

---
+++ Classes/Worker Protocols/PTCLUserNotificationSetting_Protocol.h
@@ -0,0 +1,53 @@
+//
+//  PTCLUserNotificationSetting_Protocol.h
+//  DoubleNode Core
+//
+//  Created by Darren Ehlers on 2016/10/16.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+#ifndef PTCLUserNotificationSetting_Protocol_h
+#define PTCLUserNotificationSetting_Protocol_h
+
+#import "__PTCLBase_Protocol.h"
+
+@class DAOUser;
+@class DAOUserNotificationSetting;
+
+typedef void(^PTCLUserNotificationSettingContinueBlock)();
+
+typedef void(^PTCLUserNotificationSettingBlockVoidBOOLNSError)(BOOL success, NSError* _Nullable error);
+
+typedef void(^PTCLUserNotificationSettingBlockVoidDAOUserNotificationSettingNSError)(DAOUserNotificationSetting* _Nullable user, NSError* _Nullable error);
+typedef void(^PTCLUserNotificationSettingBlockVoidNSArrayNSUIntegerNSUIntegerNSError)(NSArray<DAOUserNotificationSetting* >* _Nullable userDevices, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error);
+
+typedef void(^PTCLUserNotificationSettingBlockVoidDAOUserNotificationSettingNSErrorContinue)(DAOUserNotificationSetting* _Nullable user, NSError* _Nullable error, PTCLUserNotificationSettingContinueBlock _Nullable continueBlock);
+typedef void(^PTCLUserNotificationSettingBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)(NSArray<DAOUserNotificationSetting* >* _Nullable userDevices, NSUInteger currentPage, NSUInteger numberOfPages, NSError* _Nullable error, PTCLUserNotificationSettingContinueBlock _Nullable continueBlock);
+
+@protocol PTCLUserNotificationSetting_Protocol <PTCLBase_Protocol>
+
+@property (strong, nonatomic)   id<PTCLUserNotificationSetting_Protocol> _Nullable    nextUserNotificationSettingWorker;
+
++ (nonnull instancetype)worker;
++ (nonnull instancetype)worker:(nullable id<PTCLUserNotificationSetting_Protocol>)nextUserNotificationSettingWorker;
+
+// Single Item CRUD
+
+- (void)doLoadObjectForId:(nonnull NSString*)userNotificationSettingId
+                andUserId:(nonnull NSString*)userId
+                withBlock:(nullable PTCLUserNotificationSettingBlockVoidDAOUserNotificationSettingNSErrorContinue)block
+           andUpdateBlock:(nullable PTCLUserNotificationSettingBlockVoidDAOUserNotificationSettingNSError)updateBlock;
+
+- (void)doSaveObject:(nonnull DAOUserNotificationSetting*)userNotificationSetting
+           withBlock:(nullable PTCLUserNotificationSettingBlockVoidBOOLNSError)block;
+
+// Collection Items CRUD
+
+- (void)doLoadObjectsForUser:(nonnull DAOUser*)user
+              withParameters:(nullable NSDictionary*)parameters
+                    andBlock:(nullable PTCLUserNotificationSettingBlockVoidNSArrayNSUIntegerNSUIntegerNSErrorContinue)block
+              andUpdateBlock:(nullable PTCLUserNotificationSettingBlockVoidNSArrayNSUIntegerNSUIntegerNSError)updateBlock;
+
+@end
+
+#endif /* PTCLUserNotificationSetting_Protocol_h */
