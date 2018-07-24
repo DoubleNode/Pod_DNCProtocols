63ecd469b06ca3bd0cf6ebeba2a6485d13260e64
Darren Ehlers <me@darrenehlers.com>
2018-07-24 18:13:51 -0500

    Updated validation protocol to add configuation properties

--- Classes/Worker Protocols/PTCLValidation_Protocol.h
+++ Classes/Worker Protocols/PTCLValidation_Protocol.h
@@ -11,9 +11,31 @@
 
 #import "__PTCLBase_Protocol.h"
 
+#import "PTCLPasswordStrength_Protocol.h"
+
 @protocol PTCLValidation_Protocol <PTCLBase_Protocol>
 
 @property (strong, nonatomic)   id<PTCLValidation_Protocol> _Nullable    nextValidationWorker;
+
+@property (assign, nonatomic)   NSInteger   minimumBirthdayAge;     // -1 = no minimum
+@property (assign, nonatomic)   NSInteger   maximumBirthdayAge;     // -1 = no minimum
+
+@property (assign, nonatomic)   NSInteger   minimumHandleLength;    // -1 = no minimum
+@property (assign, nonatomic)   NSInteger   maximumHandleLength;    // -1 = no maximum
+
+@property (assign, nonatomic)   NSInteger   minimumNameLength;      // -1 = no minimum
+@property (assign, nonatomic)   NSInteger   maximumNameLength;      // -1 = no maximum
+
+@property (assign, nonatomic)   NSInteger   minimumNumberValue;
+@property (assign, nonatomic)   NSInteger   maximumNumberValue;
+
+@property (assign, nonatomic)   NSInteger   minimumPercentageValue;
+@property (assign, nonatomic)   NSInteger   maximumPercentageValue;
+
+@property (assign, nonatomic)   NSInteger   minimumUnsignedNumberValue;
+@property (assign, nonatomic)   NSInteger   maximumUnsignedNumberValue;
+
+@property (assign, nonatomic)   WKRPasswordStrengthType requiredPasswordStrength;
 
 + (nonnull instancetype)worker;
 + (nonnull instancetype)worker:(nullable id<PTCLValidation_Protocol>)nextValidationWorker;
@@ -27,6 +49,7 @@
 - (BOOL)doValidatePercentage:(nonnull NSString*)percentage error:(NSError*_Nullable*_Nullable)error;
 - (BOOL)doValidateSearch:(nonnull NSString*)search error:(NSError*_Nullable*_Nullable)error;
 - (BOOL)doValidateState:(nonnull NSString*)state error:(NSError*_Nullable*_Nullable)error;
+- (BOOL)doValidateUnsignedNumber:(nonnull NSString*)number error:(NSError*_Nullable*_Nullable)error;
 
 @end
 
